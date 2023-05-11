f4b08a4ffa61b4c05b8d2b9cc3facca1bdc6349c
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-05-11 21:04:42 +0300

    add routes/users to main 1.0

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def healthchecker(db: Session = Depends(get_db)):
 
 #app.include_router(auth.router, prefix='/api')
 #app.include_router(photos.router, prefix='/api')
-#app.include_router(users.router, prefix='/api')
+app.include_router(users.router, prefix='/api')
 
 
 
