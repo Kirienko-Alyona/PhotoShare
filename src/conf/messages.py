73f875d261e0715f041fc29750cb4ea3a40862f2
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-05-11 10:28:17 +0300

    add messages

--- src/conf/messages.py
+++ src/conf/messages.py
@@ -1,3 +1,17 @@
 # messages for
 
 # --> src.routes.auth.py && src.routes.contacts.py
+EMAIL_NOT_CONFIRMED = "Email not confirmed"
+INVALID_EMAIL = "Invalid email"
+INVALID_PASSWORD = "Invalid password"
+ACCOUNT_ALREADY_EXISTS = "Account already exists"
+USER_SUCCESSFULLY_CREATED = "User successfully created. Check your email for confirmation."
+INVALID_REFRESH_TOKEN = "Invalid refresh token"
+VERIFICATION_ERROR = "Verification error"
+YOUR_EMAIL_IS_ALREADY_CONFIRMED = "Your email is already confirmed"
+EMAIL_CONFIRMED = "Email confirmed"
+CHECK_YOUR_EMAIL_FOR_CONFIRMATION = "Check your email for confirmation."
+NOT_FOUND = "Not Found"
+INVALID_SCOPE_FOR_TOKEN = "Invalid scope for token"
+INVALID_TOKEN_FOR_EMAIL_VERIFICATION = "Invalid token for email verification"
+COULD_NOT_VALIDATE_CREDENTIALS = "Could not validate credentials"
