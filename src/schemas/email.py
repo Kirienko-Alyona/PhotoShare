70546cab135fdd036b0ea902e29771493bb4c82a
YpL68 <yuriy.plks@gmail.com>
2023-05-11 16:28:01 +0300

    email service update 1

--- src/schemas/email.py
+++ src/schemas/email.py
@@ -1,10 +1,6 @@
-from pydantic import BaseModel, EmailError, EmailStr, Field, validator, ValidationError
+from pydantic import BaseModel, EmailError, EmailStr, Field, validator
 
-from src.conf.logger import get_logger
-
-logger = get_logger(__name__)
-
-MAX_EMAIL_STR_LEN = 20  # 150
+from src.conf.constants import MAX_EMAIL_STR_LEN
 
 
 class RequestEmail(BaseModel):
@@ -23,17 +19,3 @@ class RequestEmail(BaseModel):
                 "email": "example@example.ua"
             }
         }
-
-
-if __name__ == '__main__':
-    try:
-        email = RequestEmail(email='w@w.ua')
-        print(email.email)
-
-        email = RequestEmail(email='123456werewr@gfgdfglfgjhklghjlfgjhfghfglhjfglkhjfghjgfhjfgjhfljh.com')
-        print(email.email)
-
-        email = RequestEmail(email='123456')
-        print(email.email)
-    except ValidationError as err:
-        logger.error(err)
