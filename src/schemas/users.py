b85658a74b845636fd1ae340986c01a88c3ce019
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-05-12 13:05:26 +0300

    added schemas user

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -2,12 +2,16 @@ from datetime import datetime, date
 
 from pydantic import BaseModel, Field, EmailStr
 
+from src.database.models import Role
+
 
 class UserModel(BaseModel):
-    username: str = Field(min_length=5, max_length=16)
-    email: str
+    first_name: str = Field(min_length=3, max_length=20) #'Jay'
+    username: str = Field(min_length=5, max_length=30) #'@jay_b'
+    email: EmailStr(min_length=5, max_length=50) #'jay_b@example.com'
     password: str = Field(min_length=6, max_length=10)
-
+    birthday: date = Field #'2023-03-29'
+    
 
 class UserDb(BaseModel):
     id: int
@@ -15,6 +19,7 @@ class UserDb(BaseModel):
     email: str
     created_at: datetime
     avatar: str = None
+    roles: Role
 
     class Config:
         orm_mode = True
