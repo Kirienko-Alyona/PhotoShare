d3d2078bbfce8b923f9f2a62fae14786c26eb751
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-05-12 10:05:03 +0300

    comment phone filesk and user model phone

--- src/schemas/phone_number.py
+++ src/schemas/phone_number.py
@@ -1,27 +1,27 @@
-from pydantic import BaseModel, validator
+# from pydantic import BaseModel, validator
 
-from src.services.phone_number import format_phone_num, sanitize_phone_num
-
-
-class RequestPhoneNum(BaseModel):
-    phone_number: str
-
-    @validator("phone_number")
-    def validate_phone_number(cls, phone_num):
-        try:
-            return format_phone_num(sanitize_phone_num(phone_num))
-        except ValueError:
-            raise ValueError(f"\"{phone_num}\" - is not a valid phone number")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "phone_number": "123456789"
-            }
-        }
+# from src.services.phone_number import format_phone_num, sanitize_phone_num
 
 
-# if __name__ == '__main__':
-#     phone = RequestPhoneNum(phone_number="44 54 ")
-#     # phone = RequestPhoneNum(phone_number="44 54 2--12--45")
-#     print(phone.phone_number)
+# class RequestPhoneNum(BaseModel):
+#     phone_number: str
+
+#     @validator("phone_number")
+#     def validate_phone_number(cls, phone_num):
+#         try:
+#             return format_phone_num(sanitize_phone_num(phone_num))
+#         except ValueError:
+#             raise ValueError(f"\"{phone_num}\" - is not a valid phone number")
+
+#     class Config:
+#         schema_extra = {
+#             "example": {
+#                 "phone_number": "123456789"
+#             }
+#         }
+
+
+# # if __name__ == '__main__':
+# #     phone = RequestPhoneNum(phone_number="44 54 ")
+# #     # phone = RequestPhoneNum(phone_number="44 54 2--12--45")
+# #     print(phone.phone_number)
--- src/services/phone_number.py
+++ src/services/phone_number.py
@@ -1,15 +1,15 @@
-def format_phone_num(pn: str) -> str:
-    return f"+{pn[:3]}({pn[3:5]}){pn[5:8]}-{pn[8:10]}-{pn[10:]}"
+# def format_phone_num(pn: str) -> str:
+#     return f"+{pn[:3]}({pn[3:5]}){pn[5:8]}-{pn[8:10]}-{pn[10:]}"
 
 
-def sanitize_phone_num(pn: str) -> str:
-    tel_code = {9: "380", 10: "38"}
-    snz_phone = "".join([ch for ch in pn if ch.isdecimal()])
-    if len(snz_phone) not in (9, 10, 12):
-        raise ValueError(f"Phone '{pn}' is incorrect.")
-    return tel_code.get(len(snz_phone), "") + snz_phone
+# def sanitize_phone_num(pn: str) -> str:
+#     tel_code = {9: "380", 10: "38"}
+#     snz_phone = "".join([ch for ch in pn if ch.isdecimal()])
+#     if len(snz_phone) not in (9, 10, 12):
+#         raise ValueError(f"Phone '{pn}' is incorrect.")
+#     return tel_code.get(len(snz_phone), "") + snz_phone
 
 
-# if __name__ == '__main__':
-#     p_num = "0445421245"
-#     print(format_phone_num(sanitize_phone_num(p_num)))
+# # if __name__ == '__main__':
+# #     p_num = "0445421245"
+# #     print(format_phone_num(sanitize_phone_num(p_num)))
