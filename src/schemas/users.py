97ab740d6aac70d345f069abb99167d2cfd45876
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-05-12 19:31:44 +0300

    delete config class from schemas users

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -7,11 +7,11 @@ from src.database.models import Role
 from src.conf.constants import EMAIL_MAX__LEN, EMAIL_MIN_LEN, USERNAME_MIN_LEN, USERNAME_MAX_LEN, PASSWORD_MIN_LEN, PASSWORD_MAX_LEN, FIRST_NAME_MIN_LEN, FIRST_NAME_MAX_LEN
 
 class UserModel(BaseModel):
-    first_name: str = Field(min_length=FIRST_NAME_MIN_LEN, max_length=FIRST_NAME_MAX_LEN,) #'Jay'
+    
     username: str = Field(..., min_length=USERNAME_MIN_LEN, max_length=USERNAME_MAX_LEN-1) #'@jay_b'
     email: str = Field(..., min_length=EMAIL_MIN_LEN, max_length=EMAIL_MAX__LEN) #'jay_b@example.com'
     password: str = Field(min_length=PASSWORD_MIN_LEN, max_length=PASSWORD_MAX_LEN)
-    birthday: date #'2023-03-29'
+  
     
     @validator("username")
     def validate_username(cls, username: str):
@@ -29,24 +29,18 @@ class UserModel(BaseModel):
         try:
             return EmailStr().validate(email_str)
         except EmailError:
-            raise ValueError(f"\"{email_str}\" - is not a valid email address")
-
-    class Config:
-            schema_extra = {
-                "example": {
-                    "username": "ben_345",
-                    "email": "example@example.ua"
-                }
-            }    
+            raise ValueError(f"\"{email_str}\" - is not a valid email address")    
             
         
 class UserDb(BaseModel):
     id: int
+    first_name: str = None
     username: str
     email: str
     created_at: datetime
     avatar: str = None
     roles: Role
+    birthday: date = None #'2023-03-29'
 
     class Config:
         orm_mode = True
