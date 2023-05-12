cec931d6a1517220878253a26b90324dc750255a
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-05-12 14:00:22 +0300

    add documentation

--- src/routes/photos.py
+++ src/routes/photos.py
@@ -16,6 +16,15 @@ router = APIRouter(prefix='/photos', tags=['photos'])
 async def create_photo(photo: UploadFile = File(), db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
     
+    '''
+    The create_photo function creates a new photo in the database.
+
+    :param photo: UploadFile: Upload a file to the server
+    :param db: Session: Get a database session
+    :param current_user: User: Get the user that is currently logged in
+    :return: A photo object
+    :doc-author: Trelent
+    '''
     public_id = update_filename(photo.filename)
     url = upload_photo(photo, public_id)
     photo = await repository_photos.add_photo(url, db, current_user)
