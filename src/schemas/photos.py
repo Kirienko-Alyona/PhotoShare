c726dce3f8ca1500dabb4c6a81eb72dba72ae5c4
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-05-12 21:27:44 +0300

    update photos schema

--- src/schemas/photos.py
+++ src/schemas/photos.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, Field
 
 
 class PhotoResponse(BaseModel):
     id: int
-    url_photo: str
-    description: str
+    url_photo: HttpUrl
+    description: str | None = Field(
+        default=None, title="The description of the Photo", max_length=300
+    )
 
     class Config:
         orm_mode = True
