c9580d71105eda8566f50256947662fdf8943a9d
alex_kondr <alex_kondr@outlook.com>
2023-05-13 15:50:33 +0300

    bugfix photo add add remove photo

--- src/routes/photos.py
+++ src/routes/photos.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, status, APIRouter, File, UploadFile
+from typing import Annotated
+
+from fastapi import Depends, status, APIRouter, File, UploadFile, Form, HTTPException, Path
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -6,26 +8,39 @@ from src.database.models import User
 from src.repository import photos as repository_photos
 from src.schemas.photos import PhotoResponse
 from src.services.auth import auth_service
-from src.services.photos import upload_photo, update_filename
+from src.services.photos import upload_photo
 
-
-router = APIRouter(prefix='/photos', tags=['photos'])
+router = APIRouter(prefix="/photos", tags=['photos'])
 
 
 @router.post('/', response_model=PhotoResponse, name='Create photo', status_code=status.HTTP_201_CREATED)
-async def create_photo(photo: UploadFile = File(), db: Session = Depends(get_db),
+async def create_photo(photo: Annotated[UploadFile, File()],
+                       description: Annotated[str, Form()],
+                       db: Session = Depends(get_db),
                        current_user: User = Depends(auth_service.get_current_user)):
-    
     """
     The create_photo function creates a new photo in the database.
 
+    :param description:
     :param photo: UploadFile: Upload a file to the server
     :param db: Session: Get a database session
     :param current_user: User: Get the user that is currently logged in
     :return: A photo object
     :doc-author: Trelent
     """
-    public_id = update_filename(photo.filename)
-    url = upload_photo(photo, public_id)
-    photo = await repository_photos.add_photo(url, db, current_user)
+    url = upload_photo(photo)
+    photo = await repository_photos.add_photo(url, description, db, current_user)
+    return photo
+
+
+@router.delete("/{photo_id}",
+               response_model=PhotoResponse,
+               name='Delete photo',
+               status_code=status.HTTP_202_ACCEPTED)
+async def remove_photo(photo_id: int,
+                       db: Session = Depends(get_db),
+                       current_user: User = Depends(auth_service.get_current_user)):
+    photo = await repository_photos.delete_photo(photo_id, db, current_user)
+    if photo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found")
     return photo
--- src/schemas/photos.py
+++ src/schemas/photos.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class PhotoResponse(BaseModel):
     id: int
     url_photo: str
-    # description: Optional[str]
+    description: str
 
     class Config:
         orm_mode = True
