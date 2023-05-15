eac150361410f6e5729c404ded671197b029fe18
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-05-15 09:39:03 +0300

    upadates photos patch and delete 2.0

--- src/repository/photos.py
+++ src/repository/photos.py
@@ -1,19 +1,12 @@
+from typing import Optional, List
+
 from sqlalchemy.orm import Session
 
 from src.database.models import User, Photo, Tag
 
 
 async def add_photo(url: str, description: str, db: Session, user: User):
-    """The add_photo function takes a url and adds it to the database.
-        Args:
-            url (str): The URL of the photo to be added.
-            db (Session): A connection to the database.
 
-    :param url: str: Pass the url of the photo to be added
-    :param db: Session: Pass in the database session to the function
-    :param user: User: Get the user id from the database
-    :return: A photo object
-    :doc-author: Trelent """
     photo = Photo(url_photo=url, description=description, user_id=user.id)
     db.add(photo)
     db.commit()
@@ -21,34 +14,20 @@ async def add_photo(url: str, description: str, db: Session, user: User):
     return photo
 
 
-async def get_photo(skip: int,
+async def get_photos(dict_values: dict,
+                    skip: int,
                     limit: int,
-                    photo_id: int,
-                    tags: list[str],
-                    username: str,
-                    db: Session):
-    if photo_id:
-        return db.query(Photo).filter(Photo.id == photo_id).first()
-    if tags:
-        for tag in tags:
-            return db.query(Photo).filter(Photo.tags == tag).all()
-    if username:
-        get_user: User = db.query(User).filter(User.username == username).first()
-        return db.query(Photo).filter(Photo.user_id == get_user.id).all()
-
-    return db.query(Photo).offset(skip).limit(limit).all()
-
-
-# async def description_photo_update(photo_id: int,
-#                                    new_description: str,
-#                                    db: Session,
-#                                    user: User):
-#     current_photo = db.query(Photo).filter(Photo.id == photo_id, Photo.user_id == user.id).first()
-#     if current_photo:
-#         current_photo.description = new_description
-#         db.commit()
-#         db.refresh(current_photo)
-#     return current_photo
+                    user: User,
+                    db: Session) -> Optional[List[Photo]]:
+    # if not input params - returned all list contacts
+    # else - search by parametrs: name, surname, email, phone - returned list contacts
+    contacts = db.query(Photo)
+    for key, value in dict_values.items():
+        if value is not None:
+            attr = getattr(Photo, key)
+            contacts = contacts.filter(attr.icontains(value), Photo.user_id == user.id)
+    contacts = contacts.offset(skip).limit(limit).all()
+    return contacts
 
 
 async def get_photo_by_id(photo_id: int, db: Session, user: User):
--- src/routes/photos.py
+++ src/routes/photos.py
@@ -5,7 +5,7 @@ from typing import Optional
 from src.database.db import get_db
 from src.database.models import User
 from src.repository import photos as repository_photos
-from src.schemas.photos import PhotoResponse, PhotoModel
+from src.schemas.photos import PhotoResponse
 from src.services.auth import auth_service
 from src.services.photos import upload_photo
 import src.conf.messages as messages
@@ -18,35 +18,29 @@ async def create_photo(photo: UploadFile = File(),
                        description: str | None = None,
                        db: Session = Depends(get_db),
                        current_user: User = Depends(auth_service.get_current_user)):
-    """The create_photo function creates a new photo in the database.
-    :param photo: UploadFile: Upload a file to the server
-    :param db: Session: Get a database session
-    :param current_user: User: Get the user that is currently logged in
-    :return: A photo object
-    :doc-author: Trelent """
-    # public_id = update_filename(photo.filename)
+
     url = upload_photo(photo)
     photo = await repository_photos.add_photo(url, description, db, current_user)
     return photo
 
 
-@router.get('/', response_model=PhotoResponse, name="Get photo by request ")
-async def get_photo(
-        skip: int = 0, limit: int = Query(default=10, ge=1, le=50),
-        photo_id: Optional[int] = Query(default=None),
-        tags: Optional[list] = Query(default=None),
-        username: Optional[str] = Query(default=None),
-        db: Session = Depends(get_db),
-):
-    photo = await repository_photos.get_photo(skip, limit,
-                                              photo_id, tags,
-                                              username, db)
-    if photo is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.NOT_FOUND)
-    return photo
+@router.get('/', response_model=list[PhotoResponse], name="Get photos by request ")
+async def get_photos(skip: int = 0, limit: int = Query(default=10, ge=1, le=50),
+                    tags: Optional[list] = Query(default=None),
+                    user: User = Depends(auth_service.get_current_user),
+                    db: Session = Depends(get_db)):
+    photos = await repository_photos.get_photos({'tags': tags},
+                                                skip,
+                                                limit,
+                                                user,
+                                                db)
+    if len(photos) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.NOT_FOUND)
+    return photos
 
 
-@router.patch('/photo_description/{photo_id}', response_model=PhotoResponse, name="Update photo's description")
+@router.patch('/{photo_id}', response_model=PhotoResponse, name="Update photo's description")
 async def photo_description_update(
         new_description: str,
         photo_id: int,
@@ -62,14 +56,14 @@ async def photo_description_update(
     return updated_photo
 
 
-@router.delete('/photo_delete/{photo_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def photo_delete(
+@router.delete('/{photo_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def photo_remove(
         photo_id: int,
         db: Session = Depends(get_db),
         current_user: User = Depends(auth_service.get_current_user)
 ):
     photo = await repository_photos.delete_photo(photo_id,
-                                                db, current_user)
+                                                 db, current_user)
     if photo is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.NOT_FOUND)
     return photo
