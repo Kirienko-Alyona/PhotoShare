c94b45bf42f83746d457f6ea4d1ce41089b9f90e
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-05-16 17:30:46 +0300

    commit uvicorn.run in main

--- main.py
+++ main.py
@@ -145,6 +145,6 @@ app.include_router(comments.router, prefix='/api')
 app.include_router(tags.router, prefix='/api')
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', port=8000, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run('main:app', port=8000, reload=True)
     
