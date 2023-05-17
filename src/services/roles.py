65d4ae06594b1e6b7d1a421f3a766df2c3cf3924
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-05-17 07:57:14 +0300

    del query

--- src/services/roles.py
+++ src/services/roles.py
@@ -15,11 +15,5 @@ class RoleAccess:
     async def __call__(self, request: Request, current_user: User = Depends(auth_service.get_current_user)):
         user_role = current_user.roles
         curent_role = self.allowed_operations[user_role.value]
-        # record_user_id = request.query_params.get('record_user_id', None)
-        # if record_user_id:
-        #     record_user_id = int(record_user_id)
-        #     if current_user.id != record_user_id and current_user.roles not in self.allowed_roles:
-        #         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=messages.FORBIDDEN)
-        # else:
         if self.current_operation not in curent_role:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=messages.FORBIDDEN)
