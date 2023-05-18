ae8f235e112ef073a39a7c9af17c7c705b70f96a
YpL68 <yuriy.plks@gmail.com>
2023-05-18 13:51:31 +0300

    yuri-права

--- src/repository/photo_transformations.py
+++ src/repository/photo_transformations.py
@@ -1,14 +1,13 @@
 from typing import Optional
 
+from fastapi import HTTPException, status
+
 from sqlalchemy.orm import Session
 
-from src.database.models import PhotoTransformation, Photo
+import src.conf.messages as message
+from src.database.models import PhotoTransformation, Photo, Role
 from src.schemas.photo_transformations import PhotoTransformationModel, NewDescTransformationModel
 from src.services.photo_transformations import build_transformed_url
-
-
-class RecordNotFound(Exception):
-    pass
 
 
 async def get_photo_by_id_1(photo_id: int, db: Session):
@@ -19,14 +18,30 @@ async def get_transformation_by_id(trans_id, db: Session) -> PhotoTransformation
     return db.query(PhotoTransformation).get(trans_id)
 
 
-async def create_transformation(data: PhotoTransformationModel, db: Session) -> PhotoTransformation:
-    photo = await get_photo_by_id_1(data.photo_id, db)
-    if photo is None:
-        raise RecordNotFound(f'{data.photo_id}')
+async def get_photo_user_id(photo_id: int, db: Session) -> int:  # поки Микола не зробе
+    return db.query(Photo.user_id).filter_by(id=photo_id).one()[0]
 
+
+async def get_photo_public_id(photo_id: int, db: Session) -> str:  # поки Микола не зробе
+    return db.query(Photo.cloud_public_id).filter_by(id=photo_id).one()[0]
+
+
+async def additional_rights_check(photo_id: int, cur_user_id: int,
+                                  cur_user_role: Role, db: Session, private=False):
+    photo_user_id = await get_photo_user_id(photo_id, db)
+    allowed = photo_user_id == cur_user_id if private else (cur_user_role == Role.admin or photo_user_id == cur_user_id)
+    if not allowed:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=message.FORBIDDEN)
+
+
+async def create_transformation(data: PhotoTransformationModel, user_id: int,
+                                user_role: Role, db: Session) -> Optional[PhotoTransformation]:
+    await additional_rights_check(data.photo_id, user_id, user_role, db, private=True)
+
+    public_id = await get_photo_public_id(data.photo_id, db)
     new_transformation = PhotoTransformation()
     new_transformation.photo_id = data.photo_id
-    new_transformation.transformed_url = build_transformed_url(photo.cloud_public_id, data.transformation)
+    new_transformation.transformed_url = build_transformed_url(public_id, data.transformation)
     new_transformation.description = data.description
 
     db.add(new_transformation)
@@ -37,23 +52,20 @@ async def create_transformation(data: PhotoTransformationModel, db: Session) -> 
 
 
 async def change_description(trans_id: int, data: NewDescTransformationModel,
-                             db: Session) -> Optional[PhotoTransformation]:
+                             user_id: int, user_role: Role, db: Session) -> Optional[PhotoTransformation]:
     transformation = await get_transformation_by_id(trans_id, db)
-
-    if transformation is None:
-        raise RecordNotFound(f'{trans_id}')
-
-    count = db.query(PhotoTransformation).filter_by(id=trans_id).update(
-        {'description': data.description})
-
-    if count == 1:
-        return transformation
-    return None
+    if transformation:
+        await additional_rights_check(transformation.photo_id, user_id, user_role, db)
+        count = db.query(PhotoTransformation).filter_by(id=trans_id).update(
+            {'description': data.description}, synchronize_session="fetch")
+        if count == 1:
+            return transformation
 
 
-async def remove_transformation(trans_id: int, db: Session) -> None:
+async def remove_transformation(trans_id: int, user_id: int, user_role: Role, db: Session) -> Optional[int]:
     transformation = await get_transformation_by_id(trans_id, db)
-    if transformation is None:
-        raise RecordNotFound(f'{trans_id}')
-    db.query(PhotoTransformation).filter_by(id=trans_id).delete()
-    db.commit()
+    if transformation:
+        await additional_rights_check(transformation.photo_id, user_id, user_role, db)
+        db.query(PhotoTransformation).filter_by(id=trans_id).delete(synchronize_session="fetch")
+        db.commit()
+        return transformation.id
--- src/routes/photo_transformations.py
+++ src/routes/photo_transformations.py
@@ -4,59 +4,55 @@ from sqlalchemy.orm import Session
 from src.conf.allowed_operation import allowed_operation_create, allowed_operation_update, allowed_operation_delete
 import src.conf.messages as messages
 from src.database.db import get_db
-from src.database.models import User
+from src.database.models import User, Role
 from src.repository import photo_transformations as repository_transformations
 from src.schemas.photo_transformations import (
     PhotoTransformationModelDb,
     PhotoTransformationModel,
     NewDescTransformationModel)
 from src.services.auth import auth_service
+from src.services.roles import RoleAccess
 
 router = APIRouter(prefix="/photos/transformed", tags=['photo transformations'])
+
+allowed_create = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_update = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_delete = RoleAccess([Role.admin, Role.moderator, Role.user])
 
 
 @router.post('/', response_model=PhotoTransformationModelDb,
              name='Create photo transformation', status_code=status.HTTP_201_CREATED,
-             dependencies=[Depends(allowed_operation_create)])
+             dependencies=[Depends(allowed_create)])
 async def create_transformation(transformation: PhotoTransformationModel,
                                 db: Session = Depends(get_db),
-                                _: User = Depends(auth_service.get_current_user)):
-    try:
-        transformation = await repository_transformations.create_transformation(transformation, db)
-    except repository_transformations.RecordNotFound as err:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'{messages.COULD_NOT_FIND_FOTO_BY_ID} {str(err)}')
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=messages.BAD_REQUEST)
+                                user: User = Depends(auth_service.get_current_user)):
+    transformation = await repository_transformations.create_transformation(transformation, user.id, user.roles, db)
+    if transformation is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.COULD_NOT_FIND_FOTO)
     return transformation
 
 
 @router.patch('/{trans_id}', response_model=PhotoTransformationModelDb,
               name='Changing description of transformation', status_code=status.HTTP_200_OK,
-              dependencies=[Depends(allowed_operation_update)])
+              dependencies=[Depends(allowed_update)])
 async def change_description(trans_id: int, data: NewDescTransformationModel,
                              db: Session = Depends(get_db),
-                             _: User = Depends(auth_service.get_current_user)):
-    try:
-        transformation = await repository_transformations.change_description(trans_id, data, db)
-    except repository_transformations.RecordNotFound as err:
+                             user: User = Depends(auth_service.get_current_user)):
+
+    transformation = await repository_transformations.change_description(trans_id, data, user.id, user.roles, db)
+    if transformation is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'{messages.COULD_NOT_FIND_FOTO_TRANSFORMATION_BY_ID} {str(err)}')
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=messages.BAD_REQUEST)
+                            detail=messages.COULD_NOT_FIND_FOTO_TRANSFORMATION)
     return transformation
 
 
 @router.delete("/{trans_id}",
                name='Delete photo transformation',
-               status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(allowed_operation_delete)])
+               status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(allowed_delete)])
 async def delete_transformation(trans_id: int,
                                 db: Session = Depends(get_db),
-                                _: User = Depends(auth_service.get_current_user)):
-    try:
-        await repository_transformations.remove_transformation(trans_id, db)
-    except repository_transformations.RecordNotFound as err:
+                                user: User = Depends(auth_service.get_current_user)):
+    id_ = await repository_transformations.remove_transformation(trans_id, user.id, user.roles, db)
+    if id_ is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'{messages.COULD_NOT_FIND_FOTO_TRANSFORMATION_BY_ID} {str(err)}')
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=messages.BAD_REQUEST)
+                            detail=f'{messages.COULD_NOT_FIND_FOTO_TRANSFORMATION}')
