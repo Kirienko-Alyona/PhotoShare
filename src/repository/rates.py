4f3c7ab6b1cb9f32aa4b1d80a55d5caf2c4e16a2
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2023-05-20 12:46:41 +0300

    Rate: fix calculating rate

--- src/repository/rates.py
+++ src/repository/rates.py
@@ -20,7 +20,7 @@ async def get_rate_photo_by_user(photo_id: int, db: Session, user: User):
 
 
 async def get_rating_by_photo_id(photo_id: int, db: Session):
-    result = db.query(func.count(Rate.user_id), func.avg(Rate.rate)).filter(Photo.id == photo_id).first()
+    result = db.query(func.count(Rate.user_id), func.avg(Rate.rate)).filter(Rate.photo_id == photo_id).first()
     return {'average_rate': result[1] or 0, 'rate_count': result[0]}
 
 
